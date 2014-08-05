9ba3fd73fd76cc77f9071127a7b94627a3713bc1
Johnny Appleseed <mobilemakers@Viks-MacBook-Pro.local>
2014-08-05 14:43:53 -0500

    Initial Commit

---
+++ My_Teams_Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My_Teams_Project
+//
+//  Created by Johnny Appleseed on 8/5/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
